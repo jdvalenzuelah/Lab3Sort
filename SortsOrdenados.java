216cfa2856693a317c3d0eb444c62bf5cb820184
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-02-10 02:54:52 -0600

    Version Final

--- SortsOrdenados.java
+++ SortsOrdenados.java
@@ -1,36 +1,17 @@
 import java.util.Scanner;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.BufferedReader;
-import java.io.IOException;
 
 public class SortsOrdenados {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Press any key to start...");
 		sc.next();
-
-		for(int i = 10; i < 3001; i++) {
+		for(int i = 10; i < 3001; i++){
 			System.out.println(RandomListGenerator.sortedIntFile("data.txt", i) + " Run: "+ i);
 			Integer[] list = RandomListGenerator.getData("data.txt");
-			long start = System.nanoTime();
-			MergeSort.sort(list,i);
-			long end = System.nanoTime();
-			long duration = (end - start)/1000;
-			try{
-				writeLog(i + ",QuickSort," + "VERDADERO,QuickSort.sort(java.lang.Comparable[ ], int),VERDADERO,"+duration);
-			}catch(Exception e){
-				System.out.println(e.getMessage());
-			}
+			MergeSort.sort(list, i);
+			QuickSort.sort(list, 0,i-1);
 		}
 		System.out.println("Press any key to exit...");
 		sc.next();
 	}
-
-	public static void writeLog(String line) throws java.io.IOException {
-        FileWriter file = new FileWriter("mergeSort.csv", true);
-        file.append(line+"\n");
-        file.flush();
-        file.close();
-    }
 }
